388a2866d3014396a8f7ca4f7995f91aba8d0a2b
Beni Badibanga <info.dim77@gmail.com>
2025-06-29 16:15:21 +0200

    Updated package-lock.json

--- src/components/ai-studio/ModelDesigner.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import EnhancedModelDesigner from './EnhancedModelDesigner';
-
-const ModelDesigner: React.FC = () => {
-  return <EnhancedModelDesigner />;
-};
-
-export default ModelDesigner;
